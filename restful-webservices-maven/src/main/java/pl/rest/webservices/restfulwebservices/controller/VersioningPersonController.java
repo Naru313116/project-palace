2ae1fb4fb04db77358fed750e233285e00542083
Rafał Szczepański <omega313116@gmail.com>
2024-02-11 11:26:05 +0100

    Add header and parameter and media-type versioning

--- restful-webservices-maven/src/main/java/pl/rest/webservices/restfulwebservices/controller/VersioningPersonController.java
+++ restful-webservices-maven/src/main/java/pl/rest/webservices/restfulwebservices/controller/VersioningPersonController.java
@@ -1,6 +1,7 @@
 package pl.rest.webservices.restfulwebservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.rest.webservices.restfulwebservices.model.Name;
 import pl.rest.webservices.restfulwebservices.model.PersonV1;
@@ -15,6 +16,36 @@ public class VersioningPersonController {
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondOfPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstOfPersonRequestParameter() {
+        return new PersonV1("Bon Charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstOfPersonRequestHeader() {
+        return new PersonV1("Bon Charlie");
+    }
+
+    @GetMapping(path = "/person", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstOfPersonAcceptHeader() {
+        return new PersonV1("Bon Charlie");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondOfPersonAcceptHeader() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 }
