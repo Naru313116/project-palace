e4ece10cbaa5a989b21bb1ec0bf2b89504e8ae0f
Rafał Szczepański <omega313116@gmail.com>
2024-02-11 11:27:09 +0100

    style imports

--- restful-webservices-maven/src/main/java/pl/rest/webservices/restfulwebservices/controller/VersioningPersonController.java
+++ restful-webservices-maven/src/main/java/pl/rest/webservices/restfulwebservices/controller/VersioningPersonController.java
@@ -1,7 +1,6 @@
 package pl.rest.webservices.restfulwebservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.rest.webservices.restfulwebservices.model.Name;
 import pl.rest.webservices.restfulwebservices.model.PersonV1;
