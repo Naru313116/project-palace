719194ea5ea239c602c5ab292d4176930f45f047
Rafał Szczepański <omega313116@gmail.com>
2024-02-17 23:02:39 +0100

    Add Post entity and create data

--- restful-webservices-maven/src/main/resources/data.sql
+++ restful-webservices-maven/src/main/resources/data.sql
@@ -1,6 +1,17 @@
 insert into user_details(id, birth_date, name)
-values (1001, current_date(), 'Rafal')
+values (1001, current_date(), 'Rafal');
+
 insert into user_details(id, birth_date, name)
-values (1002, current_date(), 'Naru')
+values (1002, current_date(), 'Naru');
+
 insert into user_details(id, birth_date, name)
-values (1003, current_date(), 'Jennie')
+values (1003, current_date(), 'Jennie');
+
+insert into post(id, description, user_id)
+values (2001, 'I want to learn AWS', 1001);
+
+insert into post(id, description, user_id)
+values (2002, 'I want to learn Java', 1001);
+
+insert into post(id, description, user_id)
+values (2003, 'I want to learn React', 1002);
